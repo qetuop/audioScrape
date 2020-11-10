6c7805b539b0055a131d8f314eb3600b88a6f733
qetuop <bdonson@yahoo.com>
2020-11-10 13:32:03 -0500

    adding logic to grab an entire site

--- audioScrape.py
+++ audioScrape.py
@@ -2,15 +2,64 @@ import logging
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
+import urllib.parse
 import os
 import time
 import eyed3
 import json
 import sys
 from pathvalidate import sanitize_filename
+import re
 
 DOWNLOAD_DIR = ''
 
+# given top level site, get all page urls
+def grabSite(url):
+    urls = []
+    page = None
+
+    try:
+        page = requests.get(url)
+    except:
+        logging.ERROR('Could not Parse: {}'.format(url))
+        return urls
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # get all pages for site, grab largest number
+    results = soup.find_all("a", {"class": "page-numbers"})
+    maxPage = int(1)
+    if (len(results) != 0):
+        for elem in results:
+            href = elem.get_text()
+            try:
+                maxPage = max(maxPage, int(elem.get_text()))
+            except:
+                # some other element that is not a page number, don't worry about
+                pass
+    print('maxPage:',maxPage)
+
+    # iterate over all site pages grabing each book within the page
+    currPage = 1
+    for currPage in range(1, 2):#maxPage+1):
+        currUrl = url + 'page/' + str(currPage)
+        print(currUrl)
+        try:
+            page = requests.get(currUrl)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            results = soup.find_all("h2", {"class": "entry-title"})
+            if (len(results) != 0):
+                for elem in results:
+                    urls.append(elem.a.get('href'))
+        except:
+            logging.ERROR('Could not Parse: {}'.format(url))
+
+    return urls
+
+
+
+
+# get url list from text file
 def grabUrls():
     with open("urls.txt") as file:  # Use file to refer to the file object
         data = [line.strip() for line in file.readlines() if line.strip()]
@@ -36,7 +85,7 @@ def createSaveDir(URL):
     with open(sourceFile, 'w') as file:
         file.write(URL)
 
-# Check if there are multiple pages
+# Check if there are multiple pages per book
 def grabPages(soup, pagesToScrapeList):
     # <a href="https://bigaudiobooks.net/thud/3/" class="post-page-numbers">3</a>
     results = soup.find_all(class_='post-page-numbers')
@@ -65,9 +114,12 @@ def grabAudio(URL):
     for elem in results:
         #print(elem)
         href = elem.a['href']
-        #print(href)
+        print(href)
 
-        filename = href.split('/')[-1]
+        # decode escaped strings
+        filename = urllib.parse.unquote(href.split('/')[-1]) #.encode('ascii', errors='ignore').decode("utf-8") #.encode('ascii', errors='ignore')
+        #filename = re.sub(r'[^\x00-\x7f]', r'', filename)
+        print(filename)
         localFileName = os.path.join(DOWNLOAD_DIR, filename)
         #print(localFileName)
 
@@ -115,8 +167,19 @@ if __name__ == "__main__":
     #URL = 'https://bigaudiobooks.net/thud/'  # multiple pages
     #URL = 'https://bigaudiobooks.net/the-colour-of-magic/'   # 1 page
 
-    urls = grabUrls()
+    # grab all books from an entire site
+    # TODO: add to a "sites.txt"
+    site = 'https://staraudiobook.com/series/40k/'
+    urls = grabSite(site)
     #print(urls)
+    #sys.exit(0)
+
+
+    # get urls from text file (urls.txt)
+    # TODO: add these to the ones from the sites above
+    #urls = grabUrls()
+
+    print(urls)
 
     for url in urls:
         logging.info('Parsing: {}'.format(url))
